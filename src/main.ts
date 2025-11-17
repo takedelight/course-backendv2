adbf9c24d448c158ab60b3b2e4fee3e435db7446
Nikolaenko <aleshka.dev@gmail.com>
2025-11-17 16:29:55 +0200

    git init

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: config.get<string>('CORS_ORIGIN'),
+    methods: 'GET,HEAD,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(config.getOrThrow<number>('PORT'));
+}
+bootstrap().catch((e) => console.error(e));
