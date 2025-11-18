9a856965c597a2df02c23273f8fae8a1fbcec856
Nikolaenko <aleshka.dev@gmail.com>
2025-11-18 12:21:05 +0200

    generate tickets module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     UserModule,
     AuthModule,
+    TicketModule,
   ],
 })
 export class AppModule {}
